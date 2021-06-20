ab4905d0620b9d5f22395df30de8feb2a6ff6273
Aki Van Ness <aki@lethalbit.net>
2021-06-20 14:34:14 -0400

    software: squish: Added a template action for tape I/O

---
+++ software/squish/squish/actions/tape-io.py
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: BSD-3-Clause
+from os import path
+from ..utility import *
+
+ACTION_NAME = 'tape-io'
+ACTION_DESC = 'Interact with the squishy hardware tape interface'
+
+def parser_init(parser):
+	pack_tape_img = parser.add_argument_group('tape drive IO')
+
+
+
+def action_main(args):
+
+	return 0
