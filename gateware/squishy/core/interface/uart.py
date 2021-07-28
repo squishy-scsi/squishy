3a4a4bab24f24091b9641db3cf4718b7df09d891
Aki Van Ness <aki@lethalbit.net>
2021-07-28 06:29:37 -0400

    gateware: uart: Started on the initial UART bits

--- gateware/squishy/core/interface/uart.py
+++ gateware/squishy/core/interface/uart.py
@@ -1,7 +1,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
-from nmigen                  import *
-from nmigen_stdio.serial     import AsyncSerial
-from nmigen_soc.wishbone     import Interface
+from nmigen              import *
+from nmigen.lib.fifo     import AsyncFIFO
+from nmigen_stdio.serial import AsyncSerial
+from nmigen_soc.wishbone import Interface
 
 
 __all__ = ('UARTInterface')
@@ -20,6 +21,13 @@ class UARTInterface(Elaboratable):
 
 		self._status_led = None
 
+		self._output_fifo = AsyncFIFO(
+			width    = 8,
+			depth    = 128,
+			r_domain = 'sync',
+			w_domain = 'sync',
+		)
+
 		self._uart = None
 
 	def elaborate(self, platform):
@@ -36,6 +44,51 @@ class UARTInterface(Elaboratable):
 
 		m = Module()
 
-		m.submodules += self._uart
+		uart_in  = Signal(self.config['data_bits'])
+		uart_out = Signal(self.config['data_bits'])
+
+		m.submodules += self._uart, self._output_fifo
+
+		m.d.comb += [
+			self._uart.rx.ack.eq(0)
+		]
+
+		# TODO: Handle commands w/ more than one byte
+
+		with m.FSM(reset = 'idle'):
+			with m.State('idle'):
+				m.d.sync += self._status_led.eq(0)
+
+				with m.If(self._uart.rx.rdy):
+					m.d.sync += [
+						uart_in.eq(self._uart.rx.data),
+						self._status_led.eq(1)
+					]
+
+					m.next = 'uart_ack'
+
+			with m.State('uart_ack'):
+				m.d.comb += self._uart.rx.ack.eq(1)
+				m.next = 'cmd_proc'
+
+			with m.State('cmd_proc'):
+				with m.Switch(uart_in):
+					with m.Case(0x00):
+						pass
+					with m.Default():
+						m.next = 'idle'
+
+
+
+			with m.State('data_write'):
+
+				m.next = 'idle'
+
+
+			m.d.sync += [
+				self._output_fifo.r_en.eq(self._uart.tx.rdy),
+				self._uart.tx.data.eq(self._output_fifo.r_data),
+				self._uart.tx.ack.eq(self._output_fifo.r_rdy),
+			]
 
 		return m
