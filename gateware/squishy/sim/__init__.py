f145951180ca1640b145b8f57f380ec04f98e2b3
Aki Van Ness <aki@lethalbit.net>
2021-08-04 00:47:28 -0400

    gateware: updated the UART simulation bench and replaced the dummy platform with a global simulation SimPlatform for use in the other simulation benches

--- gateware/squishy/sim/__init__.py
+++ gateware/squishy/sim/__init__.py
@@ -1,6 +1,35 @@
 # SPDX-License-Identifier: BSD-3-Clause
 __all__ = ()
 
+
+class SimPlatform():
+
+	default_clk = 'clk'
+
+	pll_config = {
+		'freq': 1e8,
+	}
+
+	# clock_domain_generator
+
+	def request(self, name, num = 0):
+		from nmigen     import Signal
+		from nmigen.hdl import Record
+
+		if name == 'led':
+			return Signal(name = f'led_{num}')
+		if name == 'uart':
+			return Record([
+				('rx', [
+					('i', 1),
+					('o', 1)
+				]),
+				('tx',[
+					('i', 1),
+					('o', 1)
+				])
+			])
+
 def sim_case(*, domains, dut):
 	def _reg_sim(func):
 		from nmigen.sim import Simulator
--- gateware/squishy/sim/uart.py
+++ gateware/squishy/sim/uart.py
@@ -2,32 +2,12 @@
 from nmigen                import *
 from nmigen.sim            import Simulator, Settle
 from nmigen.hdl.ir         import Fragment
-from nmigen.hdl            import Record
 
-from .                     import sim_case
+
+from .                     import sim_case, SimPlatform
 from ..core.interface.uart import UARTInterface
 
 SIM_NAME = 'UART'
-
-class DummyPlatform():
-	pll_config = {
-		'freq': 1e8,
-	}
-
-	def request(self, name, num = 0):
-		if name == 'led':
-			return Signal(name = f'led_{num}')
-		if name == 'uart':
-			return Record([
-				('rx', Record([
-					('i', 1),
-					('o', 1)
-				])),
-				('tx', Record([
-					('i', 1),
-					('o', 1)
-				]))
-			])
 
 _uart_config = {
 	'enabled'  : True,
@@ -47,15 +27,21 @@ _wb_cfg = {
 
 uart = Fragment.get(
 	UARTInterface(config = _uart_config, wb_config = _wb_cfg),
-	platform = DummyPlatform()
+	platform = SimPlatform()
 )
 
 @sim_case(domains = [ ('sync', 1e8) ], dut = uart)
 def sim_uart(sim, dut):
-
+	def nya():
+		from random import randint
+		yield Settle()
+		for _ in range(1024):
+			yield
+			yield
+			yield dut._uart.rx.data.eq(randint(0,255))
 
 	return [
-		()
+		(nya, 'sync')
 	]
 
 SIM_CASES = [
