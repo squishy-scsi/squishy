87df216afe93f34bf976246f8e39adcbf4fe9ef7
Aki Van Ness <aki@lethalbit.net>
2021-09-19 02:30:11 -0400

    gateware: added some code to allow for conversion from smaller time units

--- gateware/squishy/utility.py
+++ gateware/squishy/utility.py
@@ -8,6 +8,10 @@ __all__ = (
 	'inf',
 	'dbg',
 	'print_table',
+
+	'ns_to_s',
+	'us_to_s',
+	'ms_to_s',
 )
 
 def log(str, end = '\n', file = sys.stdout):
@@ -31,3 +35,16 @@ def print_table(lst, columns = 2):
 		if (idx + 1) % columns == 0:
 			print('')
 	print('')
+
+ns = 1e-9
+us = 1e-6
+ms = 1e-3
+
+def ns_to_s(val):
+	return val * ns
+
+def us_to_s(val):
+	return val * us
+
+def ms_to_s(val):
+	return val * ms
