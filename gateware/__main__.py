01810d9a8ef18eaf2cc5cc85736cde737a384c61
Aki Van Ness <aki@lethalbit.net>
2021-09-24 22:54:21 -0400

    gateware: added the deps from the host software in preparation for unification

--- gateware/__main__.py
+++ gateware/__main__.py
@@ -6,11 +6,9 @@ from pathlib import Path
 
 squishy_path = Path(sys.argv[0]).resolve()
 
-luna_dir = os.environ.get('LUNA_DIR', squishy_path.parent.parent / 'deps/luna')
+luna_dir = os.environ.get('LUNA_DIR', str(squishy_path.parent.parent / 'deps/luna'))
 
 sys.path.insert(0, luna_dir)
-
-print(luna_dir)
 
 if (squishy_path.parent / 'squishy').is_dir():
 	sys.path.insert(0, str(squishy_path.parent))
