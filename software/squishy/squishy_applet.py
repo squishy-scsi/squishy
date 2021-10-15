8bf5c05bfa1992d37862284d703f430cd3186a59
Aki Van Ness <aki@lethalbit.net>
2021-10-15 08:51:26 -0400

    software: applets: added empty applet metaclass

---
+++ software/squishy/squishy_applet.py
@@ -0,0 +1,20 @@
+# SPDX-License-Identifier: BSD-3-Clause
+from abc import ABCMeta
+
+__all__ = (
+	'SquishyApplet',
+)
+
+class SquishyApplet(metaclass = ABCMeta):
+	help         = '<HELP MISSING>'
+	description  = '<DESCRIPTION MISSING>'
+	required_rev = 'rev1'
+
+	@abstractmethod
+	def synth(self, target):
+		pass
+
+	@abstractmethod
+	def run(self, device, args):
+		pass
+
