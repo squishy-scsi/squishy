afb7ffb351f0a74d4e382672466bb6465d84b25f
Aki Van Ness <aki@lethalbit.net>
2021-10-15 09:20:51 -0400

    software: gui: some initial work on the preferences window

--- software/squishy/gui/preferences_window.py
+++ software/squishy/gui/preferences_window.py
@@ -1,13 +1,26 @@
 # SPDX-License-Identifier: BSD-3-Clause
+import json
 from os import path
 
 from PySide2.QtCore    import *
 from PySide2.QtWidgets import *
 from PySide2.QtUiTools import QUiLoader
 
+from ..config          import SQUISHY_SETTINGS_FILE
 from .widgets          import FontPicker, ColorPicker
 
 class PreferencesWindow:
+	def _fill_settings(self):
+		self.window.chk_splash.setChecked(self.settings['gui']['appearance']['show_splash'])
+
+
+	def _save_settings(self):
+		self.settings['gui']['appearance']['show_splash'] = self.window.chk_splash.checkState() == Qt.Checked
+
+
+		with open(SQUISHY_SETTINGS_FILE, 'w') as cfg:
+			json.dump(self.settings, cfg)
+
 	def __init__(self):
 		self.loader = QUiLoader()
 		self._ui_file = QFile(
@@ -20,8 +33,15 @@ class PreferencesWindow:
 		self.loader.registerCustomWidget(FontPicker)
 		self.loader.registerCustomWidget(ColorPicker)
 
+		with open(SQUISHY_SETTINGS_FILE, 'r') as cfg:
+			self.settings = json.load(cfg)
+
 		self.window = self.loader.load(self._ui_file)
 
+		self.window.btn_save.clicked.connect(self._save_settings)
+		self.window.lbl_settings.setText(f'Settings File: {SQUISHY_SETTINGS_FILE}')
+
+		self._fill_settings()
 
 	def show(self):
 		self.window.show()
