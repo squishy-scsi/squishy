f3757f8574bc3cd20910233aa947dcf5596297d2
Aki Van Ness <aki@lethalbit.net>
2021-10-21 08:33:43 -0400

    software: added some of the printer commands

--- software/squishy/scsi/commands/printer.py
+++ software/squishy/scsi/commands/printer.py
@@ -2,5 +2,39 @@
 from construct import *
 
 __all__ = (
-
+	'format_printer',
+	'print_cmd',
+	'slew_and_print',
+	'flush_buffer',
+	'recover_buffered_data',
 )
+
+# Group: 0 | Peripheral Device: Printer | Type: Optional
+format_printer = 'Format (Printer)' / BitStruct(
+	'LUN'      / BitsInteger(3),
+	'Reserved' / BitsInteger(3),
+	'Type'     / BitsInteger(2),
+	'TxLen'    / Int24ul,
+)
+
+# Group: 0 | Peripheral Device: Printer | Type: Mandatory
+print_cmd = 'Print' / BitStruct(
+	'LUN'      / BitsInteger(3),
+	'Reserved' / BitsInteger(5),
+	'TxLen'    / Int24ul,
+)
+
+# Group: 0 | Peripheral Device: Printer | Type: Optional
+slew_and_print = 'Slew and Print' / BitStruct(
+	'LUN'      / BitsInteger(3),
+	'Reserved' / BitsInteger(4),
+	'Channel'  / BitsInteger(1),
+)
+
+# Group: 0 | Peripheral Device: Printer | Type: Optional
+flush_buffer = 'Flush Buffer' / BitStruct(
+	'LUN'      / BitsInteger(3),
+	'Reserved' / BitsInteger(28),
+)
+
+# Group: 0 | Peripheral Device: Printer | Type: Optional
