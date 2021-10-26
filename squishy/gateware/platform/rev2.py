5f15756b38ae58fb013200b137a080f3ce6db48f
Aki Van Ness <aki@lethalbit.net>
2021-10-26 13:37:54 -0400

    gateware: platform: rev2: added some of the initial resources for the rev2 board

--- squishy/gateware/platform/rev2.py
+++ squishy/gateware/platform/rev2.py
@@ -40,5 +40,58 @@ class SquishyRev2(LatticeECP5Platform):
 		'clkfb_div': 25,
 	}
 
-	resources  = [ ]
-	connectors = [ ]
+	resources  = [
+		Resource('clk', 0,
+			Pins('P1', dir = 'i'),
+			Clock(16e6),
+			Attrs(GLOBAL = True, IO_STANDARD = 'SB_LVCMOS')
+		),
+		Resource('ulpi', 0,
+			Subsignal('clk',
+				Pins('P5', dir = 'i'),
+				Clock(60e6)
+			),
+			Subsignal('data',
+				Pins('P1 R2 R1 T2 T3 R3 T4 R4', dir = 'io')
+			),
+			Subsignal('dir',
+				Pins('N1', dir = 'i')
+			),
+			Subsignal('nxt',
+				Pins('P2', dir = 'i')
+			),
+			Subsignal('stp',
+				Pins('M2', dir = 'o')
+			),
+			Subsignal('rst',
+				Pins('M1', dir = 'o')
+			),
+
+			Attrs(IO_STANDARD = 'SB_LVCMOS')
+		),
+
+		*LEDResources(
+			pins = [
+				'R7', # [4] White
+				'T6', # [5] White
+				'R6', # [6] Pink
+				'P8', # [7] Pink
+				'P7', # [8] Blue
+				'P6'  # [9] Blue
+			],
+			attrs = Attrs(IO_STANDARD = 'SB_LVCMOS'),
+		),
+
+
+
+		*SPIFlashResources(0,
+			cs_n = 'R8', clk = 'N9', copi = 'T8', cipo = 'T7',
+
+			attrs = Attrs(IO_STANDARD = 'SB_LVCMOS')
+		),
+	]
+
+
+	connectors = [
+
+	]
