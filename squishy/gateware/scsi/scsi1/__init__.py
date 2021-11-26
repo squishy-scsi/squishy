f3a0c63ccd07366ed41e772da0b96cb80595bab9
Aki Van Ness <aki@lethalbit.net>
2021-11-25 22:37:51 -0500

    gateware: scsi: some reorganization of how things are layed out,

--- squishy/gateware/scsi/scsi1/__init__.py
+++ squishy/gateware/scsi/scsi1/__init__.py
@@ -1,9 +1,28 @@
 # SPDX-License-Identifier: BSD-3-Clause
+from math         import ceil, log2
 
-from .device    import Device
-from .initiator import Initiator
+from nmigen       import *
+
+from .....utility import us_to_s, ns_to_s
 
 __all__ = (
 	'Device',
 	'Initiator',
 )
+
+
+class SCSI1(Elaboratable):
+	def __init__(self, *, is_device, config):
+		self.config = config
+
+	def elaborate(self, platform):
+		m = Module()
+
+
+		return m
+
+def Device(*args, **kwargs):
+	return SCSI1(*args, is_device = True, **kwargs)
+
+def Initiator(*args, **kwargs):
+	return SCSI1(*args, is_device = False, **kwargs)
