6e4ccfb22f884e6e3eb4cd88c662a3b1d2d763e1
Aki Van Ness <aki@lethalbit.net>
2021-12-18 03:16:07 -0500

    deps: removed the vendored Luna dep

--- __main__.py
+++ __main__.py
@@ -6,10 +6,6 @@ from pathlib import Path
 
 squishy_path = Path(sys.argv[0]).resolve()
 
-luna_dir = os.environ.get('LUNA_DIR', str(squishy_path.parent / 'deps/luna'))
-
-sys.path.insert(0, luna_dir)
-
 from squishy import main
 
 if __name__ == "__main__":
--- squishy.py
+++ squishy.py
@@ -6,10 +6,6 @@ from pathlib import Path
 
 squishy_path = Path(sys.argv[0]).resolve()
 
-luna_dir = os.environ.get('LUNA_DIR', str(squishy_path.parent / 'deps/luna'))
-
-sys.path.insert(0, luna_dir)
-
 if (squishy_path.parent / 'squishy').is_dir():
 	sys.path.insert(0, str(squishy_path.parent))
 
