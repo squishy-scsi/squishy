b1bd789ab65f630a667ad2f04531539923436c0d
Aki Van Ness <aki@lethalbit.net>
2022-04-07 05:58:32 -0400

    software: Added some exceptions for squishy states and errors

---
+++ squishy/exceptions.py
@@ -0,0 +1,24 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+__all__ = (
+	'SquishyException',
+	'SquishyAppletError',
+	'SquishyDeviceError',
+	'SquishyBuildError',
+)
+
+class SquishyException(Exception):
+	""" Base class for Squishy related exceptions """
+	pass
+
+class SquishyAppletError(SquishyException):
+	""" Exceptions related to Squishy applets """
+	pass
+
+class SquishyDeviceError(SquishyException):
+	""" Exceptions related to Squishy hardware """
+	pass
+
+class SquishyBuildError(SquishyException):
+	""" Exceptions related to Squishy builds """
+	pass
