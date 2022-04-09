511d07aefe518bfd210991ce1ddba71a5ccc1071
Aki Van Ness <aki@lethalbit.net>
2022-04-09 12:33:50 -0400

    software: actions: migrated the GUI action to the new actions system

--- squishy/actions/gui.py
+++ squishy/actions/gui.py
@@ -2,9 +2,7 @@
 import logging as log
 
 from .. import __version__
-
-ACTION_NAME = 'gui'
-ACTION_DESC = 'Interactive SCSI gui'
+from .  import SquishyAction
 
 def _check_pyside():
 	try:
@@ -21,71 +19,73 @@ try:
 	from ..config          import SQUISHY_SETTINGS_FILE
 	from ..gui             import MainWindow
 	from ..gui.resources   import *
+
+	class SquishyGui:
+		def __init__(self):
+			import json
+			from os     import path
+			from signal import signal, SIGINT, SIG_DFL
+
+			QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
+			signal(SIGINT, SIG_DFL)
+
+			# load settings
+			with open(SQUISHY_SETTINGS_FILE, 'r') as cfg:
+				self.settings = json.load(cfg)
+
+			self.app = QApplication.instance() or QApplication([])
+
+			# Ensure we can find our themes/resources
+			QIcon.setThemeSearchPaths([
+				':/icons',
+			])
+
+			if self.settings['gui']['appearance']['show_splash']:
+				self.splash = QSplashScreen(
+					QPixmap(get_resource('splash.png', ResourceCategory.IMAGE, ResourceType.PATH))
+				)
+				self.splash.show()
+
+			self.main_window = MainWindow(self.settings)
+
+		def run(self):
+			self.main_window.show()
+			if self.settings['gui']['appearance']['show_splash']:
+				self.splash.finish(self.main_window.window)
+			ret = self.app.exec_()
+			# self.halt()
+			return ret
+
+		def halt(self):
+			self.app.closeAllWindows()
+
+
+	class GUI(SquishyAction):
+		pretty_name = 'Squishy Interactive GUI'
+		description = 'A more user friendly, but less polished way to use squishy'
+		short_help  = description
+
+		def register_args(self, parser):
+			pass
+
+		def run(self, args):
+			gui = SquishyGui()
+			banner = fr'''
+	------------------------------------------
+	 ####   ####  #    # #  ####  #    # #   #
+	#      #    # #    # # #      #    #  # #
+	 ####  #    # #    # #  ####  ######   #
+	     # #  # # #    # #      # #    #   #
+	#    # #   #  #    # # #    # #    #   #
+	 ####   ### #  ####  #  ####  #    #   #
+	------------------------------------------
+	################ QT5 GUI #################
+	------------------------------------------
+	squishy version: {__version__}
+	'''
+			print(banner, end = '')
+			return gui.run()
+
 except ImportError:
-	DONT_LOAD = 1
 	if not _check_pyside():
 		log.warning('To use the GUI please install PySide2')
-
-class SquishyGui:
-	def __init__(self):
-		import json
-		from os     import path
-		from signal import signal, SIGINT, SIG_DFL
-
-		QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
-		signal(SIGINT, SIG_DFL)
-
-		# load settings
-		with open(SQUISHY_SETTINGS_FILE, 'r') as cfg:
-			self.settings = json.load(cfg)
-
-		self.app = QApplication.instance() or QApplication([])
-
-		# Ensure we can find our themes/resources
-		QIcon.setThemeSearchPaths([
-			':/icons',
-		])
-
-		if self.settings['gui']['appearance']['show_splash']:
-			self.splash = QSplashScreen(
-				QPixmap(get_resource('splash.png', ResourceCategory.IMAGE, ResourceType.PATH))
-			)
-			self.splash.show()
-
-		self.main_window = MainWindow(self.settings)
-
-	def run(self):
-		self.main_window.show()
-		if self.settings['gui']['appearance']['show_splash']:
-			self.splash.finish(self.main_window.window)
-		ret = self.app.exec_()
-		# self.halt()
-		return ret
-
-	def halt(self):
-		self.app.closeAllWindows()
-
-
-
-def parser_init(parser):
-	pass
-
-def action_main(args):
-	gui = SquishyGui()
-	banner = fr'''
-------------------------------------------
- ####   ####  #    # #  ####  #    # #   #
-#      #    # #    # # #      #    #  # #
- ####  #    # #    # #  ####  ######   #
-     # #  # # #    # #      # #    #   #
-#    # #   #  #    # # #    # #    #   #
- ####   ### #  ####  #  ####  #    #   #
-------------------------------------------
-################ QT5 GUI #################
-------------------------------------------
-squishy version: {__version__}
-'''
-
-	print(banner, end = '')
-
-	return gui.run()
