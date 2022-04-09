2bbf641302f1f36b42b24ae89659071f14613309
Aki Van Ness <aki@lethalbit.net>
2022-04-09 11:41:13 -0400

    software: applets: added the stubbed analyzer applet that is to replace the default gateware action

---
+++ squishy/applets/analyzer/__init__.py
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: BSD-3-Clause
+
+from .. import SquishyApplet
+
+
+
+class Analyzer(SquishyApplet):
+	preview      = True
+	pretty_name  = 'SCSI Analyzer'
+	description  = 'SCSI Bus analyzer and replay'
+	short_help   = description
+	hardware_rev = (
+		'rev1', 'rev2'
+	)
+
+	def register_args(self, parser):
+		pass
+
+	def init_applet(self, args):
+		pass
+
+	def run(self, device, args):
+		pass
