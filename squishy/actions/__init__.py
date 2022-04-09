79e6dd90e969bb245b00131dfc2e3d493d0cb75a
Aki Van Ness <aki@lethalbit.net>
2022-04-09 12:32:26 -0400

    software: actions: started overhauling the action system based on the applet system

--- squishy/actions/__init__.py
+++ squishy/actions/__init__.py
@@ -1,3 +1,24 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-__all__ = ()
+from abc import ABCMeta, abstractmethod, abstractproperty
+
+__all__ = (
+	'SquishyAction',
+)
+
+class SquishyAction(metaclass = ABCMeta):
+	pretty_name = abstractproperty()
+	short_help  = abstractproperty()
+	help        = '<HELP MISSING>'
+	description = '<DESCRIPTION MISSING>'
+
+	def __init__(self):
+		pass
+
+	@abstractmethod
+	def register_args(self, parser):
+		raise NotImplementedError('Applets must implement this method')
+
+	@abstractmethod
+	def run(self, args):
+		raise NotImplementedError('Applets must implement this method')
