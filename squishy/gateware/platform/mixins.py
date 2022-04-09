2dec4b1ae9d656222bef5aab8665b4884e075ea5
Aki Van Ness <aki@lethalbit.net>
2022-04-09 15:01:04 -0400

    software: gateware: platform: made the mixing output a pretty progress bar

--- squishy/gateware/platform/mixins.py
+++ squishy/gateware/platform/mixins.py
@@ -1,6 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
 import logging as log
 
+from rich.progress import (
+	Progress, SpinnerColumn, BarColumn,
+	TextColumn
+)
+
 from ...cache import SquishyBitstreamCache
 
 __all__ = (
@@ -18,29 +23,37 @@ class SquishyCacheMixin:
 				build_dir = 'build', do_build = False,
 				program_opts = None, do_program = False, **kwargs):
 
-		plan = super().build(elab, name,
-				build_dir, do_build = False,
-				program_opts = program_opts, do_program = False, **kwargs)
+		with Progress(
+			SpinnerColumn(),
+			TextColumn('[progress.description]{task.description}'),
+			BarColumn(bar_width = None),
+			transient = True
+		) as progress:
+			task = progress.add_task("Building Bitstream", total=100, start=False)
 
-		if not do_build:
-			return plan
+			plan = super().build(elab, name,
+					build_dir, do_build = False,
+					program_opts = program_opts, do_program = False, **kwargs)
 
-		if not kwargs.get('skip_cache', False):
-			digest = plan.digest(size = 32).hex()
-			cache_obj = self._cache.get(digest)
+			if not do_build:
+				return plan
 
-			if cache_obj is None:
-				log.debug(f'Bitstream is not cached, building')
-				prod = plan.execute_local(build_dir)
-				self._cache.store(digest, prod, name)
+			if not kwargs.get('skip_cache', False):
+				digest = plan.digest(size = 32).hex()
+				cache_obj = self._cache.get(digest)
+
+				if cache_obj is None:
+					log.debug(f'Bitstream is not cached, building')
+					prod = plan.execute_local(build_dir)
+					self._cache.store(digest, prod, name)
+				else:
+					name = cache_obj['name']
+					prod = cache_obj['products']
 			else:
-				name = cache_obj['name']
-				prod = cache_obj['products']
-		else:
-			log.info('Skipping cache lookup')
-			prod = plan.execute_local(build_dir)
+				log.warning('Skipping cache lookup, this might take a [yellow][i]while[/][/]', extra = { 'markup': True })
+				prod = plan.execute_local(build_dir)
 
-		if not do_program:
-			return prod
+			if not do_program:
+				return prod
 
-		super().toolchain_program(prod, name, **(program_opts or {}))
+			super().toolchain_program(prod, name, **(program_opts or {}))
