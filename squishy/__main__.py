3b605e6a41d49088987aade5df86b4143ef8dee0
Aki Van Ness <aki@lethalbit.net>
2022-04-24 01:43:36 -0400

    software: fixed the location of `__main__.py`

--- squishy/__main__.py
+++ squishy/__main__.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
 # SPDX-License-Identifier: BSD-3-Clause
 import sys
-from pathlib import Path
 
-squishy_path = Path(sys.argv[0]).resolve()
-
-from squishy import main
+from . import main
 
 if __name__ == "__main__":
 	sys.exit(main())
