aaef8f8650b0762c32d03da8fe52ad0b06771d3d
Aki Van Ness <aki@lethalbit.net>
2022-04-27 06:41:14 -0400

    software: core: test: added some docs for `_MockPlatform` and `_MockRecord`

--- squishy/core/test.py
+++ squishy/core/test.py
@@ -14,7 +14,41 @@ __all__ = (
 )
 
 class _MockRecord(Record):
+	"""Mock Amaranth Record
+
+	This class is a modified version of :py:class:`Record`
+	that dynamically allocates a record for every field
+	requested by name, and caches it.
+
+	This allows :py:class:`_MockPlatform` to return an object
+	of :py:class:`_MockRecord` from ``request`` and have it
+	always be "correct".
+
+	This is done by shiming the ``__getitem__`` call and when
+	the field lookup fails to allocate a new record with the
+	requested name and return that.
+
+	..warning:: This only returns a record with 1-wide ``o`` and ``i`` fields
+
+	"""
+
 	def _insert_field(self, item):
+		"""Construct mocked Record
+
+		When the call to ``__getitem__`` runs into a field that
+		we have not mocked before, and the field is a str, then
+		we insert a mocked record with an ``i`` and ``o`` field
+		into the parent records fields.
+
+		..warning:: The field widths are currently set at 1
+
+		Parameters
+		----------
+		item : str
+			The requested field to mock
+
+		"""
+
 		self.fields[item] = Record((
 			('o', 1, DIR_FANOUT),
 			('i', 1, DIR_FANIN )
@@ -25,6 +59,18 @@ class _MockRecord(Record):
 		self.fields = {}
 
 	def __getitem__(self, item):
+		"""Return record field
+
+		If the field doesn't exist, we create it with a call
+		to ``_insert_field`` then return the generated field.
+
+		Parameters
+		----------
+		item : Any
+			The name of the requested field
+
+		"""
+
 		if isinstance(item, str):
 			try:
 				return self.fields[item]
@@ -35,7 +81,29 @@ class _MockRecord(Record):
 			return super().__getitem__(item)
 
 class _MockPlatform:
+	"""Mock Amaranth Platform
+
+	This is a mock platform that returns a :py:class:`_MockRecord`
+	whenever any call to ``request`` is made, regardless of the
+	resource being requested.
+
+	"""
+
 	def request(self, name, number = 0):
+		"""Request a resource from the platform
+
+		This call returns a :py:class:`_MockRecord` regardless
+		of any of the parameters passed in.
+
+		Parameters
+		----------
+		name : str
+			The name of the resource to request
+		number : int
+			The index of the resource to request
+
+		"""
+
 		return _MockRecord()
 
 
@@ -58,6 +126,8 @@ class SquishyGatewareTestCase(TestCase):
 		The elaboratable to test.
 	dut_args : dict[str, Any]
 		The initialization arguments for the elaboratable.
+	platform : _MockPlatform, Any
+		The platform passed to the Elaboratable DUT
 
 	'''
 
