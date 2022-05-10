9d063802b03fa73ff356013b53892f5178134187
Aki Van Ness <aki@lethalbit.net>
2022-05-09 21:37:23 -0400

    software: removed the old `squishy.py` and `squishy_gui.py` in the root of the repository in favor of using `python -m squishy` and `python -m squishy.gui`

---
+++ squishy/gui/__main__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: BSD-3-Clause
+import sys
+from pathlib import Path
+
+try:
+	from squishy import main_gui
+except ImportError:
+	squishy_path = Path(sys.argv[0]).resolve()
+
+	if (squishy_path.parent.parent / 'squishy').is_dir():
+		sys.path.insert(0, str(squishy_path.parent.parent))
+
+	from squishy import main_gui
+
+sys.exit(main_gui())
