48c44956ce2000d37a798aad7a56b7f85f763b43
Aki Van Ness <aki@lethalbit.net>
2022-09-16 07:33:13 -0400

    squishy: updated names of nox session targets

--- noxfile.py
+++ noxfile.py
@@ -9,7 +9,7 @@ BUILD_DIR = (Path.cwd() / 'build')
 DOCS_DIR  = (Path.cwd() / 'docs' / 'web')
 
 @nox.session(reuse_venv = True)
-def tests(session: nox.Session) -> None:
+def test(session: nox.Session) -> None:
 	session.run(
 		'python', '-m', 'unittest', 'discover',
 		'-s', 'tests'
@@ -24,7 +24,7 @@ def docs(session: nox.Session) -> None:
 	session.run('sphinx-build', '-b', 'html', str(DOCS_DIR), str(out_dir))
 
 @nox.session
-def lint_mypy(session: nox.Session) -> None:
+def mypy(session: nox.Session) -> None:
 	out_dir = (BUILD_DIR / 'mypy')
 	session.install('mypy')
 	session.install('lxml')
@@ -32,7 +32,7 @@ def lint_mypy(session: nox.Session) -> None:
 	session.run('mypy', '-p', 'squishy', '--html-report', str(out_dir.resolve()))
 
 @nox.session
-def lint_flake(session: nox.Session) -> None:
+def flake8(session: nox.Session) -> None:
 	session.install('flake8')
 	session.run('flake8', './squishy')
 	session.run('flake8', './tests')
