c9c129624a8fe45968591597395f72f521b6e32a
Aki Van Ness <aki@lethalbit.net>
2022-09-16 01:03:24 -0400

    squishy: setup the noxfile to run mypy, sphinx, and flake8,

--- noxfile.py
+++ noxfile.py
@@ -1,14 +1,37 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
+import shutil
+from pathlib import Path
+
 import nox
 
-nox.options.reuse_existing_virtualenvs = True
+BUILD_DIR = (Path.cwd() / 'build')
+DOCS_DIR  = (Path.cwd() / 'docs' / 'web')
 
-@nox.session
+@nox.session(reuse_venv = True)
 def tests(session: nox.Session) -> None:
-	session.create_tmp()
-	session.install('.')
 	session.run(
 		'python', '-m', 'unittest', 'discover',
 		'-s', 'tests'
 	)
+
+@nox.session
+def docs(session: nox.Session) -> None:
+	out_dir = (BUILD_DIR / 'docs')
+	shutil.rmtree(out_dir, ignore_errors = True)
+	session.install('-r', str(DOCS_DIR / 'requirements.txt'))
+	session.install('.')
+	session.run('sphinx-build', '-b', 'html', str(DOCS_DIR), str(out_dir))
+
+@nox.session
+def lint_mypy(session: nox.Session) -> None:
+	out_dir = (BUILD_DIR / 'mypy')
+	session.install('mypy')
+	session.install('lxml')
+	session.run('mypy', '-p', 'squishy', '--html-report', str(out_dir.resolve()))
+
+@nox.session
+def lint_flake(session: nox.Session) -> None:
+	session.install('flake8')
+	session.run('flake8', './squishy')
+	session.run('flake8', './tests')
