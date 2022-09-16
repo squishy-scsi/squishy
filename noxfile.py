88f86532e69f6c6601728737aa07dab339a979c1
Aki Van Ness <aki@lethalbit.net>
2022-09-16 14:36:39 -0400

    squishy: updated nox to fix up some things and get some automation for distrobution

--- noxfile.py
+++ noxfile.py
@@ -5,8 +5,22 @@ from pathlib import Path
 
 import nox
 
-BUILD_DIR = (Path.cwd() / 'build')
-DOCS_DIR  = (Path.cwd() / 'docs' / 'web')
+ROOT_DIR  = Path.cwd()
+
+BUILD_DIR = (ROOT_DIR  / 'build')
+CNTRB_DIR = (ROOT_DIR  / 'contrib')
+DOCS_DIR  = (ROOT_DIR  / 'docs')
+
+DIST_DIR     = (BUILD_DIR / 'dist')
+PKGS_DIR     = (CNTRB_DIR / 'pkg')
+DOCS_WEB_DIR = (DOCS_DIR / 'web')
+
+# Default sessions to run
+nox.options.sessions = (
+	'test',
+	'flake8',
+	'mypy'
+)
 
 @nox.session(reuse_venv = True)
 def test(session: nox.Session) -> None:
@@ -20,9 +34,9 @@ def test(session: nox.Session) -> None:
 def docs(session: nox.Session) -> None:
 	out_dir = (BUILD_DIR / 'docs')
 	shutil.rmtree(out_dir, ignore_errors = True)
-	session.install('-r', str(DOCS_DIR / 'requirements.txt'))
+	session.install('-r', str(DOCS_WEB_DIR / 'requirements.txt'))
 	session.install('.')
-	session.run('sphinx-build', '-b', 'html', str(DOCS_DIR), str(out_dir))
+	session.run('sphinx-build', '-b', 'html', str(DOCS_WEB_DIR), str(out_dir))
 
 @nox.session
 def mypy(session: nox.Session) -> None:
@@ -37,3 +51,20 @@ def flake8(session: nox.Session) -> None:
 	session.install('flake8')
 	session.run('flake8', './squishy')
 	session.run('flake8', './tests')
+
+@nox.session
+def build_dists(session: nox.Session) -> None:
+	session.install('build')
+	session.run('python', '-m', 'build',
+		'-o', str(DIST_DIR)
+	)
+
+@nox.session
+def upload_dist(session: nox.Session) -> None:
+	session.install('twine')
+	build_dists(session)
+	session.log('Uploading to PyPi')
+	session.run('python', '-m', 'twine',
+		'upload', f'{DIST_DIR}/*'
+	)
+
