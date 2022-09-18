bb949cd32b32a38ef24b540e355b9b0e4e7586b5
Aki Van Ness <aki@lethalbit.net>
2022-09-18 13:22:15 -0400

    squishy: packaging: added the version replace for the flatpak

--- contrib/pkg/flatpak/build.sh
+++ contrib/pkg/flatpak/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 VERSION=$(git describe --tag --always)
 PKG_NAME="python3-squishy-${VERSION}"
+
+sed -e "s/@SQUISHY_VERSION@/${VERSION}/g" \
+	-e "s/@SQUISHY_PKG_VERSION@/$(echo $VERSION | sed 's/-/_/g')/" \
+	moe.scsi.Squishy.yml.in > moe.scsi.Squishy.yml
