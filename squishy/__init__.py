6785826c316b652a6f582562b85f02b0c8c6e69f
Aki Van Ness <aki@lethalbit.net>
2023-06-11 13:06:23 -0400

    squishy: Updated the `__init__.py` to bounce if the python version is too old and removed the `3` from the end of `python` in the `__main__.py`

--- squishy/__init__.py
+++ squishy/__init__.py
@@ -1,4 +1,11 @@
 # SPDX-License-Identifier: BSD-3-Clause
+
+from sys import version_info
+
+# Bounce out if python is  too old
+if version_info < (3, 9):
+	raise RuntimeError('Python version 3.9  or newer is required to use Squishy')
+
 try:
 	from importlib import metadata
 	__version__ = metadata.version(__package__)
@@ -10,13 +17,13 @@ __all__ = (
 )
 
 '''\
-+=====================================+
-|                                     |
-|           !!! WARNING !!!           |
-|                                     |
-|     THIS MACHINE KNOWS NOT THE      |
-| DIFFERENCE BETWEEN METAL AND FLESH, |
-|          NOR DOES IT CARE.          |
-|                                     |
-+=====================================+
+╭─────────────────────────────────────╮
+│                                     │
+│           !!! WARNING !!!           │
+│                                     │
+│     THIS MACHINE KNOWS NOT THE      │
+│ DIFFERENCE BETWEEN METAL AND FLESH, │
+│          NOR DOES IT CARE.          │
+│                                     │
+╰─────────────────────────────────────╯
 '''
--- squishy/__main__.py
+++ squishy/__main__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # SPDX-License-Identifier: BSD-3-Clause
 import sys
 from pathlib import Path
