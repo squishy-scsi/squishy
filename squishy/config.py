05df8a854198534a46307e884dae04e20398f9e5
Aki Van Ness <aki@lethalbit.net>
2024-03-16 13:29:55 -0700

    squishy: Updated the `config.py` to better handle cross-platform paths

--- squishy/config.py
+++ squishy/config.py
@@ -1,20 +1,47 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
+import platform
 from pathlib import Path
 from os      import environ
 
 SQUISHY_NAME = 'squishy'
 
-# XDG directories
-XDG_HOME        = Path.home()                 if 'XDG_HOME'        not in environ else Path(environ['XDG_HOME'])
-XDG_CACHE_DIR   = (XDG_HOME / '.cache')       if 'XDG_CACHE_HOME'  not in environ else Path(environ['XDG_CACHE_HOME'])
-XDG_DATA_HOME   = (XDG_HOME / '.local/share') if 'XDG_DATA_HOME'   not in environ else Path(environ['XDG_DATA_HOME'])
-XDG_CONFIG_HOME = (XDG_HOME / '.config')      if 'XDG_CONFIG_HOME' not in environ else Path(environ['XDG_CONFIG_HOME'])
+_os = platform.system()
+
+if _os == 'Linux':
+	def _get_xdg_dir(name: str, default: Path) -> Path:
+		if name in environ:
+			return Path(environ[name])
+		return default
+
+	HOME_DIR   = _get_xdg_dir('XDG_HOME',        Path.home()                )
+	CACHE_DIR  = _get_xdg_dir('XDG_CACHE_HOME',  (HOME_DIR / '.cache')      )
+	DATA_DIR   = _get_xdg_dir('XDG_DATA_HOME',   (HOME_DIR / '.local/share'))
+	CONFIG_DIR = _get_xdg_dir('XDG_CONFIG_HOME', (HOME_DIR / '.config')     )
+
+	del _get_xdg_dir
+elif _os == 'Windows':
+	HOME_DIR   = Path.home()
+	CACHE_DIR  = (HOME_DIR / r'AppData\Local\Temp')
+	DATA_DIR   = (HOME_DIR / r'AppData\Roaming'   )
+	CONFIG_DIR = DATA_DIR
+elif _os == 'Darwin':
+	HOME_DIR   = Path.home()
+	CACHE_DIR  = (HOME_DIR / 'Library/Caches'             )
+	DATA_DIR   = (HOME_DIR / 'Library/Application Support')
+	CONFIG_DIR = DATA_DIR
+else:
+	HOME_DIR   = Path.home()
+	CACHE_DIR  = (HOME_DIR / '.cache'      )
+	DATA_DIR   = (HOME_DIR / '.local/share')
+	CONFIG_DIR = (HOME_DIR / '.config'     )
+
+del _os
 
 # Squishy-specific sub dirs
-SQUISHY_CACHE   = (XDG_CACHE_DIR   / SQUISHY_NAME)
-SQUISHY_DATA    = (XDG_DATA_HOME   / SQUISHY_NAME)
-SQUISHY_CONFIG  = (XDG_CONFIG_HOME / SQUISHY_NAME)
+SQUISHY_CACHE   = (CACHE_DIR  / SQUISHY_NAME)
+SQUISHY_DATA    = (DATA_DIR   / SQUISHY_NAME)
+SQUISHY_CONFIG  = (CONFIG_DIR / SQUISHY_NAME)
 
 SQUISHY_APPLETS      = (SQUISHY_DATA  / 'applets')
 SQUISHY_APPLET_CACHE = (SQUISHY_CACHE / 'applets')
