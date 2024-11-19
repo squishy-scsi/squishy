e8c47d85965040895d87addaca98b94343d49bb4
Aki Van Ness <aki@lethalbit.net>
2024-11-18 16:55:56 -0800

    squishy: gateware: bootloader: Stubbed out the SPI register map for the supervisor boot protocol

--- squishy/gateware/bootloader/rev2.py
+++ squishy/gateware/bootloader/rev2.py
@@ -4,15 +4,73 @@
 
 '''
 
-from torii               import Elaboratable, Module, Signal
-from torii.lib.fifo      import AsyncFIFO
+from torii                 import Elaboratable, Module, Signal, Record
+from torii.hdl.rec         import Direction
+from torii.lib.fifo        import AsyncFIFO
 
-from ..platform          import SquishyPlatformType
+from torii.lib.soc.csr.bus import Multiplexer, Element
 
+from ..platform            import SquishyPlatformType
+from ..peripherals.spi     import SPIInterface, SPIInterfaceMode
 
 __all__ = (
 	'Rev2',
 )
+
+
+# NOTE(aki): This needs torii main, so we're stubbing it out for now
+# class CtrlRegister(Record):
+# 	erase: Signal[1, Direction.FANOUT]
+#
+# class StatusRegister(Record):
+# 	erase_done: Signal[1, Direction.FANIN]
+
+class SupervisorRegisters(Multiplexer):
+	def __init__(self, *, name: str | None = None) -> None:
+
+		self._data_width = 8
+
+		super().__init__(addr_width = 2, data_width = self._data_width, name = name)
+
+		self._ctrl   = Element(self._data_width,     Element.Access.W,  name = 'ctrl'  )
+		self._status = Element(self._data_width,     Element.Access.R,  name = 'status')
+		self._slots  = Element(self._data_width,     Element.Access.RW, name = 'slots' )
+		self._txlen  = Element(self._data_width * 2, Element.Access.R,  name = 'txlen' )
+
+		# Overlay the Ctrl and Status registers
+		self.add(self._ctrl,   addr = 0x0)
+		self.add(self._status, addr = 0x0)
+
+		self.add(self._slots, addr = 0x1)
+		self.add(self._txlen, addr = 0x2)
+
+		self.boot_slot = Signal(4)
+		self.dest_slot = Signal(4)
+		self.txlen     = Signal(16)
+
+		# NOTE(aki): See note above
+		# self.ctrl      = CtrlRegister()
+		# self.status    = StatusRegister()
+
+		self.ctrl      = Signal(8)
+		self.status    = Signal(8)
+
+	def elaborate(self, platform: SquishyPlatformType | None) -> Module:
+		m = super().elaborate(platform)
+
+		m.d.comb += [
+			self._txlen.r_data.eq(self.txlen),
+			self._slots.r_data[0:4].eq(self.dest_slot),
+			self._slots.r_data[4:8].eq(self.boot_slot),
+			self._status.r_data.eq(self.status),
+		]
+
+		with m.If(self._ctrl.w_stb):
+			m.d.sync += [ self.ctrl.eq(self._ctrl.w_data), ]
+
+		return m
+
+
 
 class Rev2(Elaboratable):
 	'''
