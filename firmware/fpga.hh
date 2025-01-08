77e8028574460a01017a54d217203b7cfa26de1d
Aki Van Ness <aki@lethalbit.net>
2025-01-08 13:19:50 -0800

    firmware: fpga: added bitmask constants for the various register fields

--- firmware/fpga.hh
+++ firmware/fpga.hh
@@ -11,7 +11,15 @@ namespace squishy::registers {
 	constexpr inline std::uint8_t TXLEN_HIGH{2U};
 	constexpr inline std::uint8_t TXLEN_LOW{3U};
 	constexpr inline std::uint8_t IRQ{4U};
+
+	constexpr inline std::uint8_t CTRL_WRITE_DONE{1U << 0U};
+	constexpr inline std::uint8_t CTRL_IRQ_ACK{1U << 1U};
+
+	constexpr inline std::uint8_t IRQ_WANT_DFU{1U << 0U};
+	constexpr inline std::uint8_t IRQ_WRITE_SLOT{1U << 1U};
+	constexpr inline std::uint8_t IRQ_BOOT{1U << 2U};
 }
+
 
 void setup_fpga_ctrl_pins() noexcept;
 void fpga_enter_cfg() noexcept;
