2d10c0b414e2f7e61273efccf969b2b8ad31261b
Aki Van Ness <aki@lethalbit.net>
2025-07-26 07:59:50 -0700

    squishy: Removed explicit version check
    We only support Python 3.11 and newer, so we had a check in `__init__` that would fail if ran by an older version of python.
    
    But, that's not super useful anymore I suppose

--- squishy/__init__.py
+++ squishy/__init__.py
@@ -1,9 +1,4 @@
 # SPDX-License-Identifier: BSD-3-Clause
-from sys import version_info
-
-# Bounce out if python is  too old
-if version_info < (3, 11):
-	raise RuntimeError('Python version 3.11 or newer is required to use Squishy')
 
 try:
 	from importlib import metadata
